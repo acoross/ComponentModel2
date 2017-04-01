4f27c74b2fd3f1f9f24c7163f283c67cc1ec4630
Ilhwan Shin <acoross@gmail.com>
2017-04-01 17:51:07 +0900

    CerealMini for C#

---
+++ CerealMini/BinaryOutputArchive.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MessageBuilderLib;
+
+namespace CerealMini
+{
+    public class BinaryOutputArchive : IDisposable
+    {
+        MemoryStream stream;
+        BinaryWriter bw;
+
+        public BinaryOutputArchive(MemoryStream stream)
+        {
+            this.stream = stream;
+            bw = new BinaryWriter(stream);
+        }
+
+        public void Dispose()
+        {
+            if (bw != null)
+            {
+                bw.Dispose();
+                bw = null;
+                stream = null;
+            }
+        }
+
+        public void Write(char v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(short v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(ushort v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(Int32 v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(UInt32 v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(Int64 v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(UInt64 v)
+        {
+            bw.Write(v);
+        }
+        
+        public void Write(float v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(double v)
+        {
+            bw.Write(v);
+        }
+
+        public void Write(string v)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(v);
+            bw.Write((Int64)bytes.Length);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+        
+        public void Write(List<int> l)
+        {
+            bw.Write((Int64)l.Count);
+
+            int[] array = l.ToArray();
+            byte[] bytes = new byte[array.Length * sizeof(int)];
+            Buffer.BlockCopy(array, 0, bytes, 0, bytes.Length);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+        
+        public void WriteVal(object v)
+        {
+            dynamic vv = v;
+            Write(vv);
+        }
+
+        public void Write<T>(T v)
+        {
+            var attr = typeof(T).ReflectedType?.GetCustomAttributes(typeof(MessageAttribute), false);
+            if (attr == null)
+            {
+                throw new Exception();
+            }
+
+            foreach (var f in typeof(T).GetFields())
+            {
+                if (f.FieldType.IsNumeric())
+                {
+                    WriteVal(f.GetValue(v));
+                }
+                else
+                {
+                    dynamic fv = f.GetValue(v);
+                    Write(fv);
+                }
+            }
+        }
+    }
+}
